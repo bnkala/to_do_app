f4ccfb18133df950fac8ea600ec550eee352c7db
bnkala <nkala.bryan3@gmail.com>
2023-07-16 20:43:50 +0200

    restarted project

--- to_do_list/todo/urls.py
+++ to_do_list/todo/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 from . import views
-from .views import home, TaskList
+from .views import home, TaskList, TaskDetail
 
 urlpatterns = [
     path('', views.home, name='home'),
     path('tasks/', TaskList.as_view(),name='tasks'),
+    path('task/<int:pk>/',TaskDetail.as_view(),name='task'),
 ]
