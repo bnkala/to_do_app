a1cf3adaa2f3dc783e1b52b7c5804c7e094089e2
bnkala <nkala.bryan3@gmail.com>
2023-08-01 22:42:47 +0200

    Password tutorial

--- todo_list/todo/views.py
+++ todo_list/todo/views.py
@@ -57,7 +57,7 @@ class TaskList(LoginRequiredMixin, ListView):
     context_object_name = 'tasks'
 
     def get_context_data(self, **kwargs):
-        context = super().get_context_date(**kwargs)
+        context = super().get_context_data(**kwargs)
         context['tasks'] = context['tasks'].filter(user=self.request.user)
         return context
 
