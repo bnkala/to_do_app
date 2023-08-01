901d8847319efe4a1d4b9a4506b3e41ba57fff79
bnkala <nkala.bryan3@gmail.com>
2023-08-01 16:21:13 +0200

    Register fixed

--- todo_list/users/urls.py
+++ todo_list/users/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import MyLoginView
+from .views import MyLoginView, RegisterView
 from django.contrib.auth.views import LogoutView
 
 urlpatterns = [
     path('login/', MyLoginView.as_view(),name='login'),
-    path('logout/', LogoutView.as_view(next_page='login'),name='logout')
+    path('logout/', LogoutView.as_view(next_page='login'),name='logout'),
+    path('register/', RegisterView.as_view(),name='register'),
 ]
 
--- todo_list/users/views.py
+++ todo_list/users/views.py
@@ -1,9 +1,27 @@
 from django.shortcuts import render
 from django.contrib.auth.views import LoginView
 from django.urls import reverse_lazy
+from django.views.generic.edit import FormView
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login
+from django.contrib.auth.models import User
+from .forms import RegisterForm
 from django.contrib import messages
 
 # Create your views here.
+class RegisterView(FormView):
+    template_name = 'users/register.html'
+    form_class = RegisterForm
+    redirect_authenticated_user = True
+    success_url = reverse_lazy('tasks')
+
+    def form_valid(self, form):
+        user = form.save()
+        if user:
+            login(self.request, user)
+
+        return super(RegisterView, self).form_valid(form)
+
 class MyLoginView(LoginView):
     template_name= 'users/login.html'
     fields= '__all__'
